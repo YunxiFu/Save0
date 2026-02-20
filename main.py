d17ce8106c85f8cd2b16337a3bbc6736f204e187
Yunxi_Fu <18038607383@163.com>
2026-02-20 16:24:16 +0800

    0220 实现了种树、定向浇水两大核心功能

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 clear()
-from judgement_canharv import judgement
-from plant_carrot import plant_carrot
-import water
+
+import judgement_canharv 
+import plant_carrot 
+import water 
+
+target = [Entities.Carrot, Entities.Tree] # 需要浇水的作物类型列表
+
 while True:
-	for i in range(get_world_size()):
-		for j in range (get_world_size()):
-			is_harvest = judgement() # 判断是否能收割
+	for x in range(get_world_size()):
+		for y in range (get_world_size()):
+			is_harvest = judgement_canharv.judgement() # 判断是否能收割
 
-			water.water() # 浇水
+			if x == 0 or x == 5: # 第0行或第5行种植胡萝卜和树
+				if y % 2 == 0: # 偶数行种植胡萝卜
+					plant_carrot.plant_carrot() # 种植胡萝卜
 
-			if i == 0: # 第0行种植胡萝卜
-				plant_carrot()
+				else: # 奇数行种植树
+					plant(Entities.Tree)
+			
+			if y == 0 or y == 5: # 第0列或第5列种植胡萝卜和树
+				if x % 2 == 0: # 偶数列种植胡萝卜
+					plant_carrot.plant_carrot() # 种植胡萝卜
 
-			if i == 1 :
+				else: # 奇数列种植树
+					plant(Entities.Tree)
+
+			if x == 1 or x == 4: # 第1行或第4行种植灌木
 				plant(Entities.Bush)
+			
+			water.water(target) # 浇水
 
 			move(North)
 
--- water.py
+++ water.py
@@ -1,6 +1,9 @@
-def water():
-    staus = get_water()
-    while staus <= 0.25: # 水分低于25%时浇水
-        use_item(Items.Water)
-        staus = get_water()
-    return 1
+def water(target):
+	staus = get_water()
+	type = get_entity_type()
+
+	if type in target: # 如果作物类型在目标列表中
+		while staus <= 0.25: # 水分低于25%时浇水
+			use_item(Items.Water)
+			staus = get_water()
+	return 1
